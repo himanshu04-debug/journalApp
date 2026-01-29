6c18f0c33d9fef6d7457075e3011f90fda99c3b4
himanshu04-debug <hmidha04@gmail.com>
2026-01-29 23:55:14 +0530

    Add files via upload

---
+++ journalApp/src/main/java/com/himanshu/journalApp/Services/JournalEntryService.java
@@ -0,0 +1,31 @@
+package com.himanshu.journalApp.Services;
+
+import com.himanshu.journalApp.Entity.JournalEntry;
+import com.himanshu.journalApp.Repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalEntryService {
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntryRepository.save(journalEntry);
+
+    }
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
+    }
+
+    public List<JournalEntry> getAll() {
+        return journalEntryRepository.findAll();
+    }
+}
