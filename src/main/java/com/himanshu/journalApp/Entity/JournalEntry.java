478286fac29570290036e72d6594886459ae5b73
Himanshu Midha <hmidha04@gmail.com>
2026-01-31 12:26:14 +0530

    init commit

--- src/main/java/com/himanshu/journalApp/Entity/JournalEntry.java
+++ src/main/java/com/himanshu/journalApp/Entity/JournalEntry.java
@@ -1,11 +1,14 @@
 package com.himanshu.journalApp.Entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-
+@Data
 @Document(collection = "journal_entries")
 public class JournalEntry {
     @Id
@@ -14,35 +17,5 @@ public class JournalEntry {
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
