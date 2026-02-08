9bb45e30ecd6d6604874c0ff242e99d5e3dc99b6
Himanshu Midha <hmidha04@gmail.com>
2026-02-08 13:38:24 +0530

    init commit

--- src/main/java/com/himanshu/journalApp/Entity/JournalEntry.java
+++ src/main/java/com/himanshu/journalApp/Entity/JournalEntry.java
@@ -2,6 +2,7 @@ package com.himanshu.journalApp.Entity;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 public class JournalEntry {
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
