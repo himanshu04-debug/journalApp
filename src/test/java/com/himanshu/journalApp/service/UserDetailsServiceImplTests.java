1991daac4d699330e8eae9f1a28036c1e0ebfe0b
Himanshu Midha <hmidha04@gmail.com>
2026-04-11 15:07:22 +0530

    init commit

---
+++ src/test/java/com/himanshu/journalApp/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,39 @@
+package com.himanshu.journalApp.service;
+
+import com.himanshu.journalApp.Entity.User;
+import com.himanshu.journalApp.Repository.UserRepository;
+import com.himanshu.journalApp.Services.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+
+public class UserDetailsServiceImplTests {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;//initialized mocks are injected iin userdetailesServiceImpl
+
+    @Mock
+    private UserRepository userRepository;//it was not initailized.so we added before eacxh annotation
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);//mocks initialized
+    }
+    @Test
+    void loadUserByUsernameTest(){
+        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("ram").password("jkfbgsj").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+}
